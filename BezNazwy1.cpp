0b35f43cf94e112f5d54f3619a4beb042422b9ae
menakiku <sazonovamaria370@gmail.com>
2023-10-24 15:04:21 +0200

    Komentarz

---
+++ BezNazwy1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main ()
+{ 
+    short zmienna = 2;
+    cout << "Hello world!";
+    cout << endl;
+    cout << "Liczba: " << zmienna;
+    return 0;
+}
