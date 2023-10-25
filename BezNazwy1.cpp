b9f68cfe1098043be3380ad5a3f69a2d7c503e1f
menakiku <sazonovamaria370@gmail.com>
2023-10-25 15:10:33 +0200

    comentarz

--- BezNazwy1.cpp
+++ BezNazwy1.cpp
@@ -2,9 +2,39 @@
 using namespace std;
 int main ()
 { 
-    short zmienna = 2;
+	// deklaracja zmiennych
+    short liczba = 2;
+    float zmiennoprzecinkowa = 3.21;
+    string tekst = "tekst";
+    char znak = 'A';
+    bool logiczny = false;
+    
+    /*
     cout << "Hello world!";
+    cout << endl;    // koniec lini
+    cout << "Liczba: " << liczba;   // wyswietlenie zmiennej
     cout << endl;
-    cout << "Liczba: " << zmienna;
+    
+    string wyraz;
+    cout << "Podaj jakis wyraz: ";
+    cin >> wyraz;
+    cout <<"Podales wyraz: " << wyraz;
+    */
+    string imie; 
+	cout << "Podaj swoe imie";
+	cin  >> imie;
+	
+	short wiek;
+	cout << "Podaj wiek";
+	cin >> wiek;
+	 
+	
+	cout <<" Witaj " << imie << " masz " <<wiek <<" lat ";
+
+	
+	
+	
+	
     return 0;
+
 }
